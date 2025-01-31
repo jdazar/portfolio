2c057daa81bc259369d19305c0a6c8ae8ef84301
Julian Daza <jdazar@unal.edu.co>
2025-01-31 15:18:48 -0500

    feat: added deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://jdazar18.github.io',
+  base: 'portfolio',
 });
